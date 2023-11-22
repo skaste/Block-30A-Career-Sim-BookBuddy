82ae06819ce8a55249320200e1071d75a49965a7
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-22 11:09:08 -0600

    working on pulling book list from api on BookList

---
+++ BookBuddy/src/Components/BookList.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+
+const BookList = ({}) => {
+
+useEffect(() => {
+  const getBooks = () => {
+    try {
+      const response = fetch(
+
+      );
+    }
+  }
+})
+
+  return (
+    <div>BookList</div>
+  )
+}
+export default BookList
