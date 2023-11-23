c23e8a0701691b02272462a92b11ec5d0751756f
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-22 19:55:36 -0600

    made the login page need to link to App.jsx and verified the email section requires an @

--- BookBuddy/src/Components/Login.jsx
+++ BookBuddy/src/Components/Login.jsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Login = ({}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const [email, setEmail]=useState("");
-const [password, setPassword]=useState("");
-
-const handleLogin = () => {
-  fetch( 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
-  })
-}
-
+  const handleLogin = () => {
+    fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    const result = response.json();
+    console.log(result);
+    //save the token to session storage when logged in
+    sessionStorage.setToken("token", result.token);
+    //update isLoggedIn state
+    setIsLoggedIn(true);
+  };
 
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h2>Login</h2>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </>
+  );
+};
 export default Login;
